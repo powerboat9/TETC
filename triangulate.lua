810f45a360c226d833c107564389c9164c1467ca
powerboat9 <powerboat9@users.noreply.github.com>
2016-01-31 22:30:55 -0500

    Created it, did lots of math.
    My secret is copy + paste.

---
+++ triangulate.lua
@@ -0,0 +1,34 @@
+local dist, pos = ...
+
+--[[
+My Math:
+
+newPos = pos[n] + change[n]
+dist[n] = math.sqrt(change[n].x ^ 2 + change[n].y ^ 2 + change[n].z ^ 2)
+    dist[n] ^ 2 = change[n].x ^ 2 + change[n].y ^ 2 + change[n].z ^ 2
+
+Which leads to:
+
+newPos = pos[n] + change[n]
+    newPos = pos[1] + change[1]
+    newPos = pos[2] + change[2]
+    newPos = pos[3] + change[3]
+    newPos = pos[4] + change[4]
+dist[n] ^ 2 = change[n].x ^ 2 + change[n].y ^ 2 + change[n].z ^ 2
+    dist[1] ^ 2 = change[1].x ^ 2 + change[1].y ^ 2 + change[1].z ^ 2
+    dist[2] ^ 2 = change[2].x ^ 2 + change[2].y ^ 2 + change[2].z ^ 2
+    dist[3] ^ 2 = change[3].x ^ 2 + change[3].y ^ 2 + change[3].z ^ 2
+    dist[4] ^ 2 = change[4].x ^ 2 + change[4].y ^ 2 + change[4].z ^ 2
+        math.sqrt(dist[1] ^ 2 - change[1].y ^ 2 - change[1].z ^ 2) = change[1].x
+            math.sqrt(dist[1] ^ 2 - change[1].x ^ 2 - change[1].z ^ 2) = change[1].y
+            math.sqrt(dist[1] ^ 2 - change[1].x ^ 2 - change[1].y ^ 2) = change[1].z
+        math.sqrt(dist[2] ^ 2 - change[2].y ^ 2 - change[2].z ^ 2) = change[2].x
+            math.sqrt(dist[2] ^ 2 - change[2].x ^ 2 - change[2].z ^ 2) = change[2].y
+            math.sqrt(dist[2] ^ 2 - change[2].x ^ 2 - change[2].y ^ 2) = change[2].z
+        math.sqrt(dist[3] ^ 2 - change[3].y ^ 2 - change[3].z ^ 2) = change[3].x
+            math.sqrt(dist[3] ^ 2 - change[3].x ^ 2 - change[3].z ^ 2) = change[3].y
+            math.sqrt(dist[3] ^ 2 - change[3].x ^ 2 - change[3].y ^ 2) = change[3].z
+        math.sqrt(dist[4] ^ 2 - change[4].y ^ 2 - change[4].z ^ 2) = change[4].x
+            math.sqrt(dist[4] ^ 2 - change[4].x ^ 2 - change[4].z ^ 2) = change[4].y
+            math.sqrt(dist[4] ^ 2 - change[4].x ^ 2 - change[4].y ^ 2) = change[4].z
+        
