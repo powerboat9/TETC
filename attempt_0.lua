57e5bf627506c955564f12aa7a9c1d24188c8ac3
powerboat9 <powerboat9@users.noreply.github.com>
2016-04-18 09:56:10 -0400

    Added cAngle

--- attempt_0.lua
+++ attempt_0.lua
@@ -5,11 +5,12 @@ local gps3 = vector.new(tArgs[7], tArgs[8], tArgs[9])
 local gps4 = vector.new(tArgs[10], tArgs[11], tArgs[12])
 
 function trilaterate(p1, d1, p2, d2, p3, d3, p4, d4)
-    local cPoint, cDist = 1, 1
+    local cPoint, cDist, cAngle = 1, 1, 1 --temporary values
     do
         local line = p2 - p1
         local distance = d1 * ((line:length() ^ 2 + d1 ^ 2 - d2 ^ 2)/(2 * line:length() * d1))
         cPoint = line:normalize() * distance + p1
         cDist = d1 ^ 2 - distance ^ 2
+        cAngle = line:normalize()
     end
     
