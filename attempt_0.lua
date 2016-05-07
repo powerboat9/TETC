1c9b29ee7cbbd1b39295d62baa72fd546ecf3964
powerboat9 <powerboat9@users.noreply.github.com>
2016-05-06 20:25:43 -0400

    Update attempt_0.lua

--- attempt_0.lua
+++ attempt_0.lua
@@ -5,7 +5,8 @@ local gps3 = vector.new(tArgs[7], tArgs[8], tArgs[9])
 local gps4 = vector.new(tArgs[10], tArgs[11], tArgs[12])
 
 local function toDegrees(angle)
-    yDegrees = math.asin(angle.y % (math.pi / 2))
+    yDegrees = math.asin(angle.y) / math.pi * 180
+    angle.x, angle.z = angle.x / math.cos(yDegrees), angle.z / math.cos(yDegrees)
 
 local function fromDegrees(x, y)
     local newVect = vector.new(math.sin(x), 0, math.cos(x))
